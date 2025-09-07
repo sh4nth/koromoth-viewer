023f6553f389e1866d353e27c87e120509ad0aff
Shanthanu Bhardwaj <xanth@google.com>
2025-09-07 16:34:20 +0000

    Add login button to UI and display login state

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,9 +3,26 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    },
+  },
+  API: {
+    REST: {
+      "koromoth-api": {
+        endpoint: import.meta.env.VITE_API_URL,
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
